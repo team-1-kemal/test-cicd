5a67d8811128fa6eecf1214f522ac4c897aaf058
Ayat Maulana <ayatmaulana.04@gmail.com>
2022-12-12 20:37:12 +0700

    added new route

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.json({msg: 'running'}));
 app.get('/tes', (req, res) => res.json({msg: 'ci cd success'}))
+app.get('/apaya', (req, res) => res.json({msg: 'apaya'}))
 
 app.listen(8081, () => console.log('running at :8081')) 
