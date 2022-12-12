7a55938f40ff10b91160c37f7026cf823e0585dd
Ayat Maulana <ayatmaulana.04@gmail.com>
2022-12-12 20:49:47 +0700

    change root response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 
-app.get('/', (req, res) => res.json({msg: 'running'}));
+app.get('/', (req, res) => res.json({msg: 'successfuly run cicd with ilfan server and kemal server'}));
 app.get('/tes', (req, res) => res.json({msg: 'ci cd success'}))
 app.get('/apaya', (req, res) => res.json({msg: 'apaya'}))
 
